f95e838d19ae136fec3a232eec92e9727216e176
hc <heeronchang@outlook.com>
2024-06-21 11:45:45 +0800

    feat: Spring Security 自定义登录页面

--- src/main/java/com/example/springbootdemo/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/springbootdemo/configuration/SecurityConfiguration.java
@@ -4,25 +4,39 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
     @Bean
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-//                .formLogin(Customizer.withDefaults())
-                .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll())
                 .csrf(AbstractHttpConfigurer::disable)
+//                .csrf(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login/**").permitAll()
-                        .requestMatchers("/token/**").permitAll()
-                        .anyRequest().authenticated()
+                        .requestMatchers("/login/**", "/token/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+//                .formLogin(Customizer.withDefaults()) // 默认登录配置
+                .formLogin(form -> form
+                        .loginPage("/login") // 自定义登录页
+                        .loginProcessingUrl("/login") // 登录表单提交的URL
+                        .defaultSuccessUrl("/index") // 登录成功后跳转的URL
+                        .failureForwardUrl("/login?error=true") // 登录失败后跳转的URL
+                        .permitAll()
+                )
+                .logout(logout -> logout
+                        .logoutUrl("/logout") // 注销的URL
+                        .logoutSuccessUrl("/login") // 注销后跳转的URL
+                        .invalidateHttpSession(true)
+//                        .deleteCookies("JSESSIONID") // 删除指定cookie
+                )
+//                .httpBasic(Customizer.withDefaults())
                 .build();
     }
 //
